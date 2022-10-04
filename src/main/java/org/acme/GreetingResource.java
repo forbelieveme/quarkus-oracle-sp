7e42cf2c9f18eea1ab727a7eaacaf727c85711b0
forbelieveme <haner.avella@protonmail.com>
2022-10-04 01:54:27 -0500

    Implements the call for the stored procedure

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -16,6 +16,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import io.agroal.api.AgroalDataSource;
+import oracle.jdbc.OracleTypes;
 
 @Path("/hello")
 public class GreetingResource {
@@ -30,36 +31,40 @@ public class GreetingResource {
         Connection connection = null;
         ResultSet resultSet = null;
         CallableStatement cstmt = null;
-        List<Employee> employeeArray = new ArrayList<Employee>();
+        List<Employee> employeesArray = new ArrayList<Employee>();
 
         try {
 
             connection = dataSource.getConnection();
 
-            // resultSet = connection.createStatement().executeQuery("call
-            // SP_CONSULTA_GENERALES('','','','','','','',?)");
+            cstmt = connection.prepareCall("BEGIN SP_CONSULTA_GENERALES(?,?,?,?,?,?,?,?); END;");
+            cstmt.setString(1, "Test1");
+            cstmt.setString(2, "Test2");
+            cstmt.setString(3, "Test3");
+            cstmt.setString(4, "Test4");
+            cstmt.setString(5, "Test5");
+            cstmt.setString(6, "Test6");
+            cstmt.setString(7, "Test7");
+            cstmt.registerOutParameter(8, OracleTypes.CURSOR);
+            cstmt.execute();
 
-            // // System.out.println(resultSet);
-            // while (resultSet.next()) {
-            // for (int i = 1; i <= 2; i++) {
-            // if (i > 1) System.out.print(", ");
-            // String columnValue = resultSet.getString(i);
-            // // System.out.print(columnValue + " " + rsmd.getColumnName(i));
-            // System.out.print(columnValue);
-            // }
-            // System.out.println("");
-            // }
+            resultSet = (ResultSet) cstmt.getObject(8);
+
+            while (resultSet.next()) {
+
+                Employee employee = new Employee(resultSet.getString("ID"),
+                        resultSet.getString("NAME"),
+                        resultSet.getString("SALARY"), resultSet.getString("DPTO"),
+                        resultSet.getString("LOCATION"));
+                employeesArray.add(employee);
+            }
+
         } catch (Exception e) {
             // TODO: handle exception
             System.out.println(e);
         }
-        Employee employee1 = new Employee("A1", "Jon", "Doe", "Managment", "NY");
-        Employee employee2 = new Employee("A2", "Jon2", "Doe2", "Managment", "NY");
 
-        employeeArray.add(employee1);
-        employeeArray.add(employee2);
-
-        return Response.ok(employeeArray).build();
+        return Response.ok(employeesArray).build();
 
     }
 }
