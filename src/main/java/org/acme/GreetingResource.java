b5b498c2c64371834eeb8cb56d2518a0724d7904
forbelieveme <haner.avella@protonmail.com>
2022-10-04 01:19:52 -0500

    Returns two serialized Employee objects as json

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,7 +1,11 @@
 package org.acme;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -9,6 +13,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import io.agroal.api.AgroalDataSource;
 
@@ -19,29 +24,42 @@ public class GreetingResource {
     AgroalDataSource dataSource;
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() throws SQLException {
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response hello() throws SQLException {
+
+        Connection connection = null;
+        ResultSet resultSet = null;
+        CallableStatement cstmt = null;
+        List<Employee> employeeArray = new ArrayList<Employee>();
+
         try {
 
-            Connection connection = dataSource.getConnection();
+            connection = dataSource.getConnection();
 
-            java.sql.ResultSet resultSet = connection.createStatement().executeQuery("select * from test");
+            // resultSet = connection.createStatement().executeQuery("call
+            // SP_CONSULTA_GENERALES('','','','','','','',?)");
 
-            // System.out.println(resultSet);
-            while (resultSet.next()) {
-                for (int i = 1; i <= 2; i++) {
-                    if (i > 1) System.out.print(",  ");
-                    String columnValue = resultSet.getString(i);
-                    // System.out.print(columnValue + " " + rsmd.getColumnName(i));
-                    System.out.print(columnValue);
-                }
-                System.out.println("");
-            }
+            // // System.out.println(resultSet);
+            // while (resultSet.next()) {
+            // for (int i = 1; i <= 2; i++) {
+            // if (i > 1) System.out.print(", ");
+            // String columnValue = resultSet.getString(i);
+            // // System.out.print(columnValue + " " + rsmd.getColumnName(i));
+            // System.out.print(columnValue);
+            // }
+            // System.out.println("");
+            // }
         } catch (Exception e) {
             // TODO: handle exception
             System.out.println(e);
         }
-        return "Hello from RESTEasy Reactive";
+        Employee employee1 = new Employee("A1", "Jon", "Doe", "Managment", "NY");
+        Employee employee2 = new Employee("A2", "Jon2", "Doe2", "Managment", "NY");
+
+        employeeArray.add(employee1);
+        employeeArray.add(employee2);
+
+        return Response.ok(employeeArray).build();
 
     }
 }
