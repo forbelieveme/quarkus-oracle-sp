20a8cb4c336baa7d1dd47d0f26142036a55e722c
forbelieveme <haner.avella@protonmail.com>
2022-10-13 17:45:03 -0500

    Creates the class that describes the body´s input with it's parameters

---
+++ src/main/java/org/acme/StoredProcedureRequestBody.java
@@ -0,0 +1,82 @@
+package org.acme;
+
+public class StoredProcedureRequestBody {
+
+    private String parameter_1;
+    private String parameter_2;
+    private String parameter_3;
+    private String parameter_4;
+    private String parameter_5;
+    private String parameter_6;
+    private String parameter_7;
+
+    public StoredProcedureRequestBody(String parameter_1, String parameter_2, String parameter_3, String parameter_4,
+            String parameter_5, String parameter_6, String parameter_7) {
+        this.parameter_1 = parameter_1;
+        this.parameter_2 = parameter_2;
+        this.parameter_3 = parameter_3;
+        this.parameter_4 = parameter_4;
+        this.parameter_5 = parameter_5;
+        this.parameter_6 = parameter_6;
+        this.parameter_7 = parameter_7;
+    }
+
+    public String getParameter_1() {
+        return parameter_1;
+    }
+
+    public void setParameter_1(String parameter_1) {
+        this.parameter_1 = parameter_1;
+    }
+
+    public String getParameter_2() {
+        return parameter_2;
+    }
+
+    public void setParameter_2(String parameter_2) {
+        this.parameter_2 = parameter_2;
+    }
+
+    public String getParameter_3() {
+        return parameter_3;
+    }
+
+    public void setParameter_3(String parameter_3) {
+        this.parameter_3 = parameter_3;
+    }
+
+    public String getParameter_4() {
+        return parameter_4;
+    }
+
+    public void setParameter_4(String parameter_4) {
+        this.parameter_4 = parameter_4;
+    }
+
+    public String getParameter_5() {
+        return parameter_5;
+    }
+
+    public void setParameter_5(String parameter_5) {
+        this.parameter_5 = parameter_5;
+    }
+
+    public String getParameter_6() {
+        return parameter_6;
+    }
+
+    public void setParameter_6(String parameter_6) {
+        this.parameter_6 = parameter_6;
+    }
+
+    public String getParameter_7() {
+        return parameter_7;
+    }
+
+    public void setParameter_7(String parameter_7) {
+        this.parameter_7 = parameter_7;
+    }
+
+    
+
+}
