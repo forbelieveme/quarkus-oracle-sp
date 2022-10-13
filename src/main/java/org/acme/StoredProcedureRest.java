2f25b2c194dc2ae33ba154d304f9a73de0f4ae83
forbelieveme <haner.avella@protonmail.com>
2022-10-13 17:45:44 -0500

    Extracts the logic to another class

--- src/main/java/org/acme/StoredProcedureRest.java
+++ src/main/java/org/acme/StoredProcedureRest.java
@@ -1,29 +1,16 @@
 package org.acme;
 
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.inject.Inject;
-
-// import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import io.agroal.api.AgroalDataSource;
-import oracle.jdbc.OracleTypes;
 
 @Path("/employees")
 public class StoredProcedureRest {
 
-    @Inject
-    AgroalDataSource dataSource;
+    // @Inject
+    // AgroalDataSource dataSource;
 
     // @GET
     // @Produces(MediaType.APPLICATION_JSON)
@@ -62,7 +49,6 @@ public class StoredProcedureRest {
     // }
 
     // } catch (Exception e) {
-    // // TODO: handle exception
     // System.out.println(e);
     // }
 
@@ -70,95 +56,70 @@ public class StoredProcedureRest {
 
     // }
 
-
     // @GET
     // @Produces(MediaType.APPLICATION_JSON)
     // public Response hello() throws SQLException {
 
-    //     Connection connection = null;
-    //     ResultSet resultSet = null;
-    //     CallableStatement cstmt = null;
-    //     List<Test> employeesArray = new ArrayList<Test>();
+    // Connection connection = null;
+    // ResultSet resultSet = null;
+    // CallableStatement cstmt = null;
+    // List<Test> employeesArray = new ArrayList<Test>();
 
-    //     try {
+    // try {
 
-    //         connection = dataSource.getConnection();
+    // connection = dataSource.getConnection();
 
-    //         cstmt = connection.prepareCall("BEGIN SP_APL_DETALLE_SOLICITUD(?,?); END;");
-    //         cstmt.setString(1, "40839");
-    //         cstmt.registerOutParameter(2, OracleTypes.CURSOR);
-    //         cstmt.execute();
+    // cstmt = connection.prepareCall("BEGIN SP_APL_DETALLE_SOLICITUD(?,?); END;");
+    // cstmt.setString(1, "40839");
+    // cstmt.registerOutParameter(2, OracleTypes.CURSOR);
+    // cstmt.execute();
 
-    //         resultSet = (ResultSet) cstmt.getObject(2);
+    // resultSet = (ResultSet) cstmt.getObject(2);
 
-    //         while (resultSet.next()) {
+    // while (resultSet.next()) {
 
-    //             Test employee = new Test(
-    //                     resultSet.getString("ID_INSTANCIA"),
-    //                     resultSet.getString("DEPARTAMENTO"),
-    //                     resultSet.getString("TIPO_AJUSTE"),
-    //                     resultSet.getString("MONEDA"),
-    //                     resultSet.getString("FECHA_CREACION"),
-    //                     resultSet.getString("FECHA_PROCESADO"),
-    //                     resultSet.getString("ID_INSTANCIA_RELACIONADA"),
-    //                     resultSet.getString("APROBACION_ADJUNTA"),
-    //                     resultSet.getString("TOTAL_DEBITO"),
-    //                     resultSet.getString("TOTAL_CREDITO"),
-    //                     resultSet.getString("TOTAL_TRANSACCIONES"),
-    //                     resultSet.getString("ESTADO_SOLICITUD"),
-    //                     resultSet.getString("NIVEL_APROBACION"),
-    //                     resultSet.getString("CREADOR"),
-    //                     resultSet.getString("REVISOR"),
-    //                     resultSet.getString("APROBADOR"));
-    //             employeesArray.add(employee);
-    //         }
+    // Test employee = new Test(
+    // resultSet.getString("ID_INSTANCIA"),
+    // resultSet.getString("DEPARTAMENTO"),
+    // resultSet.getString("TIPO_AJUSTE"),
+    // resultSet.getString("MONEDA"),
+    // resultSet.getString("FECHA_CREACION"),
+    // resultSet.getString("FECHA_PROCESADO"),
+    // resultSet.getString("ID_INSTANCIA_RELACIONADA"),
+    // resultSet.getString("APROBACION_ADJUNTA"),
+    // resultSet.getString("TOTAL_DEBITO"),
+    // resultSet.getString("TOTAL_CREDITO"),
+    // resultSet.getString("TOTAL_TRANSACCIONES"),
+    // resultSet.getString("ESTADO_SOLICITUD"),
+    // resultSet.getString("NIVEL_APROBACION"),
+    // resultSet.getString("CREADOR"),
+    // resultSet.getString("REVISOR"),
+    // resultSet.getString("APROBADOR"));
+    // employeesArray.add(employee);
+    // }
 
-    //     } catch (Exception e) {
-    //         // TODO: handle exception
-    //         System.out.println(e);
-    //     }
+    // } catch (Exception e) {
+    // System.out.println(e);
+    // }
 
-    //     return Response.ok(employeesArray).build();
+    // return Response.ok(employeesArray).build();
 
     // }
 
     @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response hello() throws SQLException {
+    public Response hello(StoredProcedureRequestBody SPRequestBody) {
 
-        // Connection connection = null;
-        // ResultSet resultSet = null;
-        // CallableStatement cstmt = null;
-        List<Employee> employeeArray = new ArrayList<Employee>();
+        List<Employee> response;
 
         try {
+            StoredProcedureCall spcall = new StoredProcedureCall();
 
-            // connection = dataSource.getConnection();
+            response = spcall.callStoredProcedure(SPRequestBody);
 
-            // resultSet = connection.createStatement().executeQuery("call
-            // SP_CONSULTA_GENERALES('','','','','','','',?)");
-
-            // // System.out.println(resultSet);
-            // while (resultSet.next()) {
-            // for (int i = 1; i <= 2; i++) {
-            // if (i > 1) System.out.print(", ");
-            // String columnValue = resultSet.getString(i);
-            // // System.out.print(columnValue + " " + rsmd.getColumnName(i));
-            // System.out.print(columnValue);
-            // }
-            // System.out.println("");
-            // }
+            return Response.ok(response).build();
         } catch (Exception e) {
-            // TODO: handle exception
-            System.out.println(e);
+            return Response.status(500).entity(e).build();
         }
-        Employee employee1 = new Employee("A1", "Jon", "Doe", "Managment", "NY");
-        Employee employee2 = new Employee("A2", "Jon2", "Doe2", "Managment", "NY");
-
-        employeeArray.add(employee1);
-        employeeArray.add(employee2);
-
-        return Response.ok(employeeArray).build();
 
     }
 }
