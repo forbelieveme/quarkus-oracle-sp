22a0d85ceee62ba266e8d15e9c6f099eef7df562
forbelieveme <haner.avella@protonmail.com>
2022-10-13 17:44:00 -0500

    Creates the class that calls the stored procedure

---
+++ src/main/java/org/acme/StoredProcedureCall.java
@@ -0,0 +1,54 @@
+package org.acme;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import io.agroal.api.AgroalDataSource;
+import oracle.jdbc.OracleTypes;
+
+public class StoredProcedureCall {
+
+    @Inject
+    AgroalDataSource oracleDataSource;
+
+    Connection connection = null;
+    ResultSet resultSet = null;
+    CallableStatement cstmt = null;
+    List<Employee> employeesArray = new ArrayList<Employee>();
+
+    public List<Employee> callStoredProcedure(StoredProcedureRequestBody SPRequestBody) throws SQLException {
+
+        connection = oracleDataSource.getConnection();
+
+        cstmt = connection.prepareCall("BEGIN SP_CONSULTA_GENERALES(?,?,?,?,?,?,?,?); END;");
+        cstmt.setString(1, SPRequestBody.getParameter_1());
+        cstmt.setString(2, SPRequestBody.getParameter_2());
+        cstmt.setString(3, SPRequestBody.getParameter_3());
+        cstmt.setString(4, SPRequestBody.getParameter_4());
+        cstmt.setString(5, SPRequestBody.getParameter_5());
+        cstmt.setString(6, SPRequestBody.getParameter_6());
+        cstmt.setString(7, SPRequestBody.getParameter_7());
+        cstmt.registerOutParameter(8, OracleTypes.CURSOR);
+        cstmt.execute();
+
+        resultSet = (ResultSet) cstmt.getObject(8);
+
+        while (resultSet.next()) {
+
+            Employee employee = new Employee(resultSet.getString("ID"),
+                    resultSet.getString("NAME"),
+                    resultSet.getString("SALARY"), resultSet.getString("DPTO"),
+                    resultSet.getString("LOCATION"));
+            employeesArray.add(employee);
+        }
+
+        return employeesArray;
+
+    }
+}
