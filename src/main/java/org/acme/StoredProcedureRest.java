970fcf9c5c0208579fa5780d260c979dbc8a80d4
forbelieveme <haner.avella@protonmail.com>
2022-10-19 13:16:30 -0500

    Creates the datasource injection and passes it as an argument

--- src/main/java/org/acme/StoredProcedureRest.java
+++ src/main/java/org/acme/StoredProcedureRest.java
@@ -2,12 +2,20 @@ package org.acme;
 
 import java.util.List;
 
+import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import io.agroal.api.AgroalDataSource;
+import io.quarkus.agroal.DataSource;
+
 @Path("/employees")
 public class StoredProcedureRest {
+
+    @Inject
+    @DataSource("db1")
+    AgroalDataSource oracleDataSource;
 
     @POST
     public Response hello(StoredProcedureRequestBody SPRequestBody) {
@@ -17,7 +25,7 @@ public class StoredProcedureRest {
         try {
             StoredProcedureCall spcall = new StoredProcedureCall();
 
-            response = spcall.callStoredProcedure(SPRequestBody);
+            response = spcall.callStoredProcedure(SPRequestBody, oracleDataSource);
 
             return Response.ok(response).build();
         } catch (Exception e) {
